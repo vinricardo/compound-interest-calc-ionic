63763fbbaced8eea79c5470ee214a233a2529422
Vinicius Ricardo <vinicius.01deagosto@gmail.com>
2021-11-16 21:00:20 -0300

    feature: initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  form: FormGroup;
+  result: any;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() {}
+  ngOnInit(){
+    this.createForm();
+  }
+
+  createForm(){
+    this.form = this.fb.group({
+      value: [null,Validators.required],
+      rate: [null,Validators.required],
+      term: [null,Validators.required]
+    })
+  }
+
+  calculate(){
+    const value = this.money.value * (1 + ((this.rate.value/100) * this.term.value));
+    this.result = value.toFixed(2);
+  }
+  
+  clearForm(){
+    this.form.reset();
+    this.result = null;
+  }
+
+  get money(): AbstractControl{
+    return this.form.get('value')!;
+  }
+
+  get rate(): AbstractControl{
+    return this.form.get('rate')!;
+  }
+
+  get term(): AbstractControl{
+    return this.form.get('term')!;
+  }
+
 
 }
